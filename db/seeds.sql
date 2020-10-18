6b116a141c3a000b6a51ac247e0585d15e1b3ed5
Dylan Nguyen <babydylannguyen@gmail.com>
2020-10-17 19:52:44 -0700

    db setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Double-Double w/ grilled onions");
+INSERT INTO burgers (burger_name) VALUES ("Big Mac");
+INSERT INTO burgers (burger_name) VALUES ("Whopper");
